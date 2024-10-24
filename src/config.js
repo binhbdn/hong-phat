df9ae6a69fd57fb7856d79510ce0e9d429644f7d
binhbdn <binh.buidn@gmail.com>
2024-10-24 16:32:25 +0700

    update info

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const phoneNumber = "0988957584";
-export const emailAddress = "hongphat@gmail.com";
+export const phoneNumber = "0977343936";
+export const emailAddress = "hongphatngukim@gmail.com";
 export const mapUrl = "https://maps.app.goo.gl/v4CwwvEsFtDqs1zz5";
