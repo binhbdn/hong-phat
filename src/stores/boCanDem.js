c20a11874d3eb2ece613332efaabaec92472a2df
binhbdn <binh.buidn@gmail.com>
2024-12-08 15:11:33 +0700

    cap nhat 2 san pham bo can dem

--- src/stores/boCanDem.js
+++ src/stores/boCanDem.js
@@ -7,12 +7,12 @@ const items = [
     category: "pBoCanDem",
     routeName: "pBoCanDemCT",
     isTypical: true,
-    code: "BCD_1",
-    images: ["/img/logo.png"],
+    code: "BCD85_18",
+    images: ["/bo_can_dem_can_mau_ga_kep/bo_can_dem_85_18.webp"],
     extraImages: null,
     name: {
-      vi: "Bộ căn đệm 1 vi?",
-      cn: "Bộ căn đệm 1 cn?"
+      vi: "Bộ căn đệm song song 18 chi tiết - chiều dày 8.5mm",
+      cn: "平行垫片套装18件 - 厚度8.5mm"
     },
     prices: {
       current: null,
@@ -20,13 +20,13 @@ const items = [
       max: 0
     },
     description: {
-      vi: "Description vi?",
-      cn: "Description cn?"
+      vi: "Dùng để đệm, căn chỉnh phôi",
+      cn: "用于垫片和工件对准"
     },
     descriptions: null,
-    seri: "seri?",
-    specs: "specs?",
-    brand: "brand?",
+    seri: "8.5mm",
+    specs: "(18 chi tiết)",
+    brand: "Vân Tập",
     origin: ["cn"],
     listView: listViewCBO,
     detailsView: null
@@ -36,12 +36,12 @@ const items = [
     category: "pBoCanDem",
     routeName: "pBoCanDemCT",
     isTypical: false,
-    code: "BCD_2",
-    images: ["/img/logo.png"],
+    code: "BCD90_18",
+    images: ["/bo_can_dem_can_mau_ga_kep/bo_can_dem_90_18.webp"],
     extraImages: null,
     name: {
-      vi: "Bộ căn đệm 2 vi?",
-      cn: "Bộ căn đệm 2 cn?"
+      vi: "Bộ căn đệm song song 18 chi tiết - chiều dày 9mm",
+      cn: "平行垫片套装18件 - 厚度9mm"
     },
     prices: {
       current: null,
@@ -49,13 +49,13 @@ const items = [
       max: 0
     },
     description: {
-      vi: "Description vi?",
-      cn: "Description cn?"
+      vi: "Dùng để đệm, căn chỉnh phôi",
+      cn: "用于垫片和工件对准"
     },
     descriptions: null,
-    seri: "seri?",
-    specs: "specs?",
-    brand: "brand?",
+    seri: "9mm",
+    specs: "(18 chi tiết)",
+    brand: "Vân Tập",
     origin: ["cn"],
     listView: listViewCBO,
     detailsView: null
