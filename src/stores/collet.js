4ea1e696d690f29338b8631351a0ba9cbc8ab099
binhbdn <binh.buidn@gmail.com>
2024-12-17 17:26:12 +0700

    add collet ER32

--- src/stores/collet.js
+++ src/stores/collet.js
@@ -8,6 +8,99 @@ const items = [
   {
     category: "pCollet",
     routeName: "pColletCT",
+    isTypical: true,
+    code: "ER32",
+    images: [
+      "/collet/ER32/vt_1.webp",
+      "/collet/ER32/vt_spec.webp",
+      "/collet/ER32/vt_spec_1.webp",
+      // "/collet/ER32/vt_spec_2.webp",
+      "/collet/ER32/vt_2.webp",
+      "/collet/ER/vt_3.webp"
+    ],
+    extraImages: [
+      {
+        title: "specifications",
+        subTitle: null,
+        preview: false,
+        options: "",
+        data: ["/collet/ER32/vt_spec.webp"]
+      }
+    ],
+    name: {
+      vi: "Collet ER32 - Đầu kẹp dao máy CNC",
+      cn: "ER32筒夹 - 数控机床刀具夹头"
+    },
+    prices: {
+      current: 0,
+      min: 92000,
+      max: 150000
+    },
+    description: {
+      vi: "Dùng cho đầu kẹp CNC ER32",
+      cn: "用于ER32数控夹头"
+    },
+    descriptions: null,
+    seri: "ER32",
+    specs: "D1-D20",
+    brand: "KON",
+    origin: ["cn"],
+    listView: listViewAiDB,
+    // extra data
+    detailsView,
+    accuracy: "0.005/0.008mm",
+    innerDiameter: "D1-D20",
+    colletInnerDiameter: "D1-D20",
+    specsList: [
+      "ER32-1±0.008mm",
+      "ER32-1.5±0.008mm",
+      "ER32-2±0.008mm",
+      "ER32-2.5±0.008mm",
+      "ER32-3±0.005mm",
+      "ER32-3±0.008mm",
+      "ER32-3.175±0.005mm",
+      "ER32-3.175±0.008mm",
+      "ER32-4±0.005mm",
+      "ER32-4±0.008mm",
+      "ER32-5±0.005mm",
+      "ER32-5±0.008mm",
+      "ER32-6±0.005mm",
+      "ER32-6±0.008mm",
+      "ER32-7±0.005mm",
+      "ER32-7±0.008mm",
+      "ER32-8±0.005mm",
+      "ER32-8±0.008mm",
+      "ER32-9±0.005mm",
+      "ER32-9±0.008mm",
+      "ER32-10±0.005mm",
+      "ER32-10±0.008mm",
+      "ER32-11±0.005mm",
+      "ER32-11±0.008mm",
+      "ER32-12±0.005mm",
+      "ER32-12±0.008mm",
+      "ER32-12.7±0.005mm",
+      "ER32-12.7±0.008mm",
+      "ER32-13±0.005mm",
+      "ER32-13±0.008mm",
+      "ER32-14±0.005mm",
+      "ER32-14±0.008mm",
+      "ER32-15±0.005mm",
+      "ER32-15±0.008mm",
+      "ER32-16±0.005mm",
+      "ER32-16±0.008mm",
+      "ER32-17±0.005mm",
+      "ER32-17±0.008mm",
+      "ER32-18±0.005mm",
+      "ER32-18±0.008mm",
+      "ER32-19±0.005mm",
+      "ER32-19±0.008mm",
+      "ER32-20±0.005mm",
+      "ER32-20±0.008mm"
+    ]
+  },
+  {
+    category: "pCollet",
+    routeName: "pColletCT",
     isTypical: false,
     code: "ER16",
     images: [
@@ -16,7 +109,7 @@ const items = [
       "/collet/ER16/vt_spec_1.webp",
       "/collet/ER16/vt_spec_2.webp",
       "/collet/ER16/vt_2.webp",
-      "/collet/ER16/vt_3.webp"
+      "/collet/ER/vt_3.webp"
     ],
     extraImages: [
       {
@@ -81,7 +174,7 @@ const items = [
   {
     category: "pCollet",
     routeName: "pColletCT",
-    isTypical: true,
+    isTypical: false,
     code: "ER11",
     images: [
       "/collet/ER11/vt_1.webp",
@@ -89,7 +182,7 @@ const items = [
       "/collet/ER11/vt_spec_1.webp",
       "/collet/ER11/vt_spec_2.webp",
       "/collet/ER11/vt_2.webp",
-      "/collet/ER11/vt_3.webp"
+      "/collet/ER/vt_3.webp"
     ],
     extraImages: [
       {
