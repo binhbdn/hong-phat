6c0429b597b5758959b1bdf0765f84eff98c2b68
binhbdn <binh.buidn@gmail.com>
2024-12-27 20:38:35 +0700

    them mui doa MDH7

--- src/stores/giaCongLo.js
+++ src/stores/giaCongLo.js
@@ -104,8 +104,63 @@ const items = [
     detailsView,
     // extra data
     specsList: ["MT3", "MT4", "MT5"]
+  },
+  {
+    category: "pGiaCongLo",
+    routeName: "pGiaCongLoCT",
+    isTypical: true,
+    code: "MDH7",
+    images: ["/gia_cong_lo/mui_doa/vt_1.webp", "/gia_cong_lo/mui_doa/vt_spec.webp", "/gia_cong_lo/mui_doa/vt_2.webp"],
+    extraImages: [
+      {
+        title: "specifications",
+        subTitle: null,
+        preview: true,
+        options: "?w=450",
+        data: ["/gia_cong_lo/mui_doa/vt_spec.webp"]
+      }
+    ],
+    name: {
+      vi: "Mũi doa 3H7-10H7",
+      cn: "3H7-10H7铰刀"
+    },
+    prices: {
+      current: 0,
+      min: 56000,
+      max: 128000
+    },
+    description: {
+      vi: "Dùng để dưỡng trục thử lỗ H7",
+      cn: "用于检验H7孔的轴规"
+    },
+    descriptions: {
+      vi: [
+        {
+          title: "description",
+          data: [
+            "Mũi doa là mũi khoan lỗ để gia công các lỗ ghép chính xác đến IT5 với độ bóng bề mặt cao.",
+            "Dùng cho máy khoan, máy phay, máy doa",
+            "Được chế tạo bằng thép gió, có lưỡi cắt làm bằng hợp kim cứng hoặc kim cương đa tinh thể."
+          ]
+        }
+      ],
+      cn: [
+        {
+          title: "description",
+          data: ["铰刀是一种用于加工高精度到IT5的配合孔的钻头、表面光洁度高。", "适用于钻床、铣床、铰床。", "由高速钢制成，切削刃由硬质合金或多晶金刚石制成。"]
+        }
+      ]
+    },
+    seri: "MDH7",
+    specs: "3H7-10H7",
+    brand: "",
+    origin: ["cn"],
+    listView: listViewCBO,
+    detailsView,
+    // extra data
+    specsList: ["3H7", "4H7", "5H7", "6H7", "7H7", "8H7", "9H7", "10H7"]
   }
-  // TODO: add more 5 items
+  // TODO: add more 4 items
 ];
 
 export default items;
